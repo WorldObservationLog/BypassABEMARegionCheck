c676db5baa015fdde9963a3b9f87d6bc142f80a2
WorldObservationLog <wolc@duck.com>
2024-06-22 03:57:52 +0800

    feat: support version 10.82.0 and version 100.65.0 (TV)

--- app/src/main/java/moe/wol/bypassabemaregioncheck/MainHook.kt
+++ app/src/main/java/moe/wol/bypassabemaregioncheck/MainHook.kt
@@ -1,12 +1,26 @@
 package moe.wol.bypassabemaregioncheck
 
+import android.os.Build
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
+import java.io.File
+
 
 class MainHook: IXposedHookLoadPackage {
+    private val ANDROID_VERSION_CLASSNAME_MAPPING = mapOf(
+        2120000 to mapOf("Success" to "xi.a\$b", "DefaultIpCheckApi" to "Hh.f\$a", "Root" to "pu.n"),
+        2120004 to mapOf("Success" to "Ai.a\$b", "DefaultIpCheckApi" to "Jh.f\$a", "Root" to "Nu.m")
+    )
+    private val TV_VERSION_CLASSNAME_MAPPING = mapOf(
+        2110001 to mapOf("RegionCheckRepositoryImpl" to "pb.V", "IPCheckEntity" to "Ab.L0", "countryCodeEntity" to "Ab.X",
+            "divisionType" to "Bb.j", "Root" to "Pc.O", "Continuation" to "D8.d"),
+        2110018 to mapOf("RegionCheckRepositoryImpl" to "ec.H0", "IPCheckEntity" to "oc.T0", "countryCodeEntity" to "oc.d0",
+            "divisionType" to "pc.j", "Root" to "Dd.C", "Continuation" to "o9.d")
+    )
+
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam?) {
         if (!lpparam?.packageName.equals("tv.abema")) return
         hook(lpparam)
@@ -14,21 +28,31 @@ class MainHook: IXposedHookLoadPackage {
 
     private fun hook(lpparam: XC_LoadPackage.LoadPackageParam?) {
         val tv = XposedHelpers.findClassIfExists("pb.V", lpparam?.classLoader) != null
+        val versionCode = this.getAppVersionCode(lpparam)
+        val classname = if (tv) {
+            TV_VERSION_CLASSNAME_MAPPING[versionCode]
+        } else {
+            ANDROID_VERSION_CLASSNAME_MAPPING[versionCode]
+        }
+        if (classname == null) {
+            XposedBridge.log("Unsupported version!")
+            return
+        }
         if (tv) {
             val classLoader = lpparam?.classLoader
-            val interfaceC0880d = classLoader?.loadClass("D8.d")
-            XposedHelpers.findAndHookMethod("pb.V", lpparam?.classLoader, "c", interfaceC0880d, object : XC_MethodHook() {
+            val interfaceC0880d = classLoader?.loadClass(classname["Continuation"])
+            XposedHelpers.findAndHookMethod(classname["RegionCheckRepositoryImpl"], lpparam?.classLoader, "c", interfaceC0880d, object : XC_MethodHook() {
                 override fun beforeHookedMethod(param: MethodHookParam) {
-                    val ipCheckEntity = XposedHelpers.findClass("Ab.L0", lpparam?.classLoader)
-                    val countryCodeEntity = XposedHelpers.findClass("Ab.X", lpparam?.classLoader)
+                    val ipCheckEntity = XposedHelpers.findClass(classname["IPCheckEntity"], lpparam?.classLoader)
+                    val countryCodeEntity = XposedHelpers.findClass(classname["countryCodeEntity"], lpparam?.classLoader)
                     val countryCodeJP = XposedHelpers.newInstance(countryCodeEntity, "JP")
-                    val divisionType = XposedHelpers.findClass("Bb.j", lpparam?.classLoader)
+                    val divisionType = XposedHelpers.findClass(classname["divisionType"], lpparam?.classLoader)
                     val divisionJAPAN = XposedHelpers.getStaticObjectField(divisionType, "c")
                     val ipCheckEntityResult = XposedHelpers.newInstance(ipCheckEntity, countryCodeJP, divisionJAPAN)
                     param.result = ipCheckEntityResult
                 }
             })
-            XposedHelpers.findAndHookMethod("Pc.O", lpparam?.classLoader, "a", object : XC_MethodHook() {
+            XposedHelpers.findAndHookMethod(classname["Root"], lpparam?.classLoader, "a", object : XC_MethodHook() {
                 override fun beforeHookedMethod(param: MethodHookParam) {
                     param.result = false
                 }
@@ -36,7 +60,7 @@ class MainHook: IXposedHookLoadPackage {
         } else {
             val classLoader = lpparam?.classLoader
             val obj = classLoader?.loadClass("java.lang.Object")
-            XposedHelpers.findAndHookMethod("Hh.f\$a", lpparam?.classLoader, "invokeSuspend", obj, object : XC_MethodHook() {
+            XposedHelpers.findAndHookMethod(classname["DefaultIpCheckApi"], lpparam?.classLoader, "invokeSuspend", obj, object : XC_MethodHook() {
                 override fun beforeHookedMethod(param: MethodHookParam) {
                     XposedBridge.log(param.args.toString())
                     val division = XposedHelpers.findClass("tv.abema.protos.Division", lpparam?.classLoader)
@@ -45,16 +69,33 @@ class MainHook: IXposedHookLoadPackage {
                     val f85841e = XposedHelpers.getStaticObjectField(c18457h, "e")
                     val getIPCheckResponse = XposedHelpers.findClass("tv.abema.protos.GetIPCheckResponse", lpparam?.classLoader)
                     val getIPCheckResponseJP = XposedHelpers.newInstance(getIPCheckResponse, "JP", "Asia/Tokyo", "+09:00", "https://ds-linear-abematv.akamaized.net/region", divisionNONE, f85841e)
-                    val success = XposedHelpers.findClass("xi.a\$b", lpparam?.classLoader)
+                    val success = XposedHelpers.findClass(classname["Success"], lpparam?.classLoader)
                     val successJP = XposedHelpers.newInstance(success, getIPCheckResponseJP)
                     param.args[0] = successJP
                 }
             })
-            XposedHelpers.findAndHookMethod("pu.n", lpparam?.classLoader, "a", object : XC_MethodHook() {
+            XposedHelpers.findAndHookMethod(classname["Root"], lpparam?.classLoader, "a", object : XC_MethodHook() {
                 override fun beforeHookedMethod(param: MethodHookParam) {
                     param.result = false
                 }
             })
         }
+    }
+
+    private fun getAppVersionCode(lpparam: XC_LoadPackage.LoadPackageParam?): Int {
+        val apkPath = File(lpparam!!.appInfo.sourceDir)
+        val versionCode = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val pkgParserClass =
+                XposedHelpers.findClass("android.content.pm.PackageParser", lpparam.classLoader)
+            val packageLite =
+                XposedHelpers.callStaticMethod(pkgParserClass, "parsePackageLite", apkPath, 0)
+            XposedHelpers.getIntField(packageLite, "versionCode")
+        } else {
+            val parserCls =
+                XposedHelpers.findClass("android.content.pm.PackageParser", lpparam.classLoader)
+            val pkg = XposedHelpers.callMethod(parserCls.newInstance(), "parsePackage", apkPath, 0)
+            XposedHelpers.getIntField(pkg, "mVersionCode")
+        }
+        return versionCode
     }
 }
